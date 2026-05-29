be5ce1961881f30bbe6d2be6e6c2f9ee2abcac60
BulandaK <eskimosl@wp.pl>
2026-05-29 17:21:39 +0200

    init commit

---
+++ src/main/java/com/example/personalBudget/PersonalBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetApplication.class, args);
+	}
+
+}
